ef505ebee1df3bf366d29a4aa12739de3281c4fa
Pedro Paulo <ppaulomoreira22@gmail.com>
2024-03-04 22:29:53 -0300

    media.py

--- media.py
+++ media.py
@@ -2,10 +2,6 @@ print("Digite nota 1")
 entrada1 =      int(input())
 print("Digite nota 2")
 entrada2 =      int(input()) 
-print("Digite nota 3")
-entrada3 =      int(input())
-print("Digite nota 4")
-entrada4 =      int(input())
 
-soma = entrada1 + entrada2 + entrada3 + entrada4
-print(soma/4)
+soma = entrada1 + entrada2 
+print(soma/2)
